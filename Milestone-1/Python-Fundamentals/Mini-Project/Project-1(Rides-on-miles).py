ca36a27eaf623f431e4d9094e57e1bb252fb2e79
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-06-29 00:29:35 +0530

    Project-1(Rides-on-miles).py

--- Milestone-1/Python-Fundamentals/Mini-Project/Project-1(Rides-on-miles).py
+++ Milestone-1/Python-Fundamentals/Mini-Project/Project-1(Rides-on-miles).py
@@ -7,7 +7,7 @@ Description: Create a python program that asks the user how far they want to tra
 """
 
 
-distance = float(input("How far would you like to travel in miles?"))
+distance = float(input("How far would you like to travel in miles?"))  #because distance can be a decimal value
 
 if distance < 3:
     print("I suggest Bicycle to your destination")
@@ -15,3 +15,13 @@ elif 3 <= distance < 300:
     print("I suggest Motor-Cycle to your destination")
 else:
     print("I suggest Super-Car to your destination")
+
+
+
+"""
+Concepts Used:
+- Conditional Statements (if, elif, else)
+- User Input (input())
+- Data Type Casting (float()) 
+
+"""
