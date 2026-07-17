2da7e4c0d3e27257b63f1b35797c9b1cb44cdfe2
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-07-17 23:36:09 +0530

    Create Check if a Key Exists.py

---
+++ Milestone-1/Data-Structure/DS-Assignment/Dictionary-Question/Check if a Key Exists.py
@@ -0,0 +1,13 @@
+"""
+Description: 
+Checks for the presence of a specific key in a dictionary 
+using the 'in' operator and prints an appropriate message.
+"""
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+key_to_check = 'b'
+
+if key_to_check in my_dict:
+    print(f"The key '{key_to_check}' exists in the dictionary.")
+else:
+    print(f"The key '{key_to_check}' does not exist.")
