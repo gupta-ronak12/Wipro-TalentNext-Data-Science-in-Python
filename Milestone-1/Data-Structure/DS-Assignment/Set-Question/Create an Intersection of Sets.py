4ea60e127a511cb6ced8eabf3713ce3b1ee006c7
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-07-24 23:52:06 +0530

    Create Create an Intersection of Sets.py

---
+++ Milestone-1/Data-Structure/DS-Assignment/Set-Question/Create an Intersection of Sets.py
@@ -0,0 +1,13 @@
+"""
+Description: 
+Finds the common elements between two sets using the 'intersection()' 
+method or the '&' operator.
+"""
+
+set1 = {10, 20, 30, 40}
+set2 = {30, 40, 50, 60}
+
+
+result_set = set1 & set2
+
+print("Intersection of sets:", result_set)
