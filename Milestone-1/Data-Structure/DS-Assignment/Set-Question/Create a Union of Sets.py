6871a282ec1c4565c79dc6b21e2884efb0543903
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-07-24 23:53:59 +0530

    Create Create a Union of Sets.py

---
+++ Milestone-1/Data-Structure/DS-Assignment/Set-Question/Create a Union of Sets.py
@@ -0,0 +1,13 @@
+"""
+Description: 
+Combines all unique elements from two sets using the 'union()' 
+method or the '|' operator.
+"""
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+
+result_set = set1 | set2
+
+print("Union of sets:", result_set)
